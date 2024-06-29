8a4701d3d22f9ae1883ea9ec8ef949dd56a16cad
Finley Wood <finley@Finleys-MacBook-Pro.local>
2024-06-29 01:59:46 +0100

    Theming and rooter settings

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
