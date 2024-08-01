0fc7b82313aebfa1ad44e9b214f5a1ad37159e0a
finleywood <finleyjohnwood@gmail.com>
2024-08-01 11:45:42 +0100

    added mappings for closing and closing buffers

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "-", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>c", ":bd!<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>Q", ":q!<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>W", ":w!<CR>")
