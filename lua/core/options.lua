6eaf50870089ef3eb878c50f578aeda356bbdc46
finleywood <finleyjohnwood@gmail.com>
2024-08-05 17:12:24 +0100

    small changes to options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,1 +1,17 @@
-vim.wo.number = true
+vim.opt.nu = true
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
